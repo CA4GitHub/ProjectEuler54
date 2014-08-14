ee336d6902ae1f1c6f4beaf2f2e29bb94ee9c43b
CA4GitHub <flawton@fredricsmacbook.att.net>
2014-08-14 09:33:30 -0700

    Fixed bug in convertToDictionary
    Bug caused convertToDictionary to return just a single card’s suit and
    rank.
    Now it returns the suit and rank for all 5 cards.

--- ProjectEuler54.py
+++ ProjectEuler54.py
@@ -1,15 +1,30 @@
 def convertToDictionary(pokerHandList):
+        
     pokerHandDict = {}
-    for card in pokerHandList:
-        pokerHandDict['rank'] = card[0]
-        pokerHandDict['suit'] = card[1]
-        print('The rank is {0} and the suit is {1}'.format(pokerHandDict['rank'],pokerHandDict['suit']))
+    pokerHandListDict = []
+    for i in range(len(pokerHandList)):
+        pokerHandDict['rank'] = pokerHandList[i][0]
+        pokerHandDict['suit'] = pokerHandList[i][1]
+        pokerHandListDict.append(pokerHandDict)
+
+    print(pokerHandListDict)
+    return pokerHandDict
+
 
 pokerFile = open('poker.txt', encoding = 'utf-8')
+
+numPlayer1Wins = 0
+numPlayer2Wins = 0
+
 for line in pokerFile:
 
     aPokerHand = (line.rstrip()).split(' ')
-    player1PokerHandList = aPokerHand[0:4]
-    player2PokerHandList = aPokerHand[5:9]
-    player1PokerHandDict = convertToDictionary(player1PokerHandList)
     
+    player1PokerHandList = aPokerHand[0:5]
+    player2PokerHandList = aPokerHand[5:10]
+  
+    player1PokerHandDict = convertToDictionary(player1PokerHandList)
+    player2PokerHandDict = convertToDictionary(player2PokerHandList)
+'''    
+    winner = playPoker(player1PokerHandDict, player2PokerHandDict)
+    '''
